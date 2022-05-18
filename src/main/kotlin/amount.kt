468dd0f0d857fcb36fefc176b4a884922a31fbbc
romanx241 <hohlovr@mail.ru>
2022-05-18 12:07:16 +0300

    Add Autotest

--- src/main/kotlin/amount.kt
+++ src/main/kotlin/amount.kt
@@ -1,64 +1,71 @@
 import java.util.*
 
 
-var limitPerevod = false
+fun main() {
 
-
-fun main(args: Array<String>) {
-
-    var amount = 0
-    var sumpredperevod = 0
-    var pay = 0.0
+    val amount: Int
+    val sumpredperevod: Int
     val `in` = Scanner(System.`in`)
     print("Введите тип карты: ")
     val typeCard = `in`.nextLine()
     print("Введите сумму перевода в рублях: ")
     amount = `in`.nextInt()
-    amount *= 100
     print("Введите сумму предыдущих переводов: ")
     sumpredperevod = `in`.nextInt()
-    sumpredperevod *= 100
-    pay = analizCard(typeCard, amount, sumpredperevod)
-    if (limitPerevod === true) {
-        println("Сумма перевода в копейках составит: " + amount + "коп.")
-        println("С вас будет взята комиссия в размере : $pay коп.")
-        println("Итоговая сумма в копейках составит: " + (amount + pay) + " коп.")
-        println("Итоговая сумма в рублях составит: " + (amount + pay) / 100 + " руб.")
-    } else {
-        println("Превышен лимит перевода")
-    }
+    analizCard(typeCard, amount, sumpredperevod)
+    analizLimitPerevod(typeCard, amount, sumpredperevod)
+
 }
 
 
-fun analizCard(card: String, sumperevod: Int, sumpredperevod: Int): Double {
-    limitPerevod = true
+fun analizCard(card: String, sumperevod: Int, sumpredperevod: Int): Int {
+
+    val sumperevod = sumperevod * 100
+    val limitPerevod = true
+    var pay = 0
+    if ((card == "Mastercard" || card == "Maestro") && sumperevod <= 7500000) {
+        pay = 0
+    }
+    if ((card == "Mastercard" || card == "Maestro") && sumperevod > 7500000) {
+        pay = (sumperevod * 0.6 / 100 + 2000).toInt()
+    }
+    if (card == "Visa" || card == "Мир") {
+        pay = (sumperevod * 0.75 / 100).toInt()
+        if (pay < 3500) {
+            pay = 3500
+        }
+    }
+    if (card == "VKPay") {
+        pay = 0
+    }
+
+    if (limitPerevod === true) {
+        println("Сумма перевода в копейках составит: " + sumperevod + "коп.")
+        println("С вас будет взята комиссия в размере: $pay коп.")
+        println("Итоговая сумма в копейках составит: " + (sumperevod + pay) + " коп.")
+        println("Итоговая сумма в рублях составит: " + (sumperevod + pay) / 100 + " руб.")
+    } else {
+        println("Превышен лимит перевода")
+    }
+    return pay
+}
+
+fun analizLimitPerevod(card: String, sumperevod: Int, sumpredperevod: Int): Boolean {
+
+    val sumperevod = sumperevod * 100
+    var limitPerevod = true
     if (card == "Mastercard" || card == "Maestro" || card == "Visa" || card == "Мир") {
         if (sumperevod > 15000000 || sumperevod + sumpredperevod > 60000000) {
             limitPerevod = false
-            return 0.0
         }
     }
     if (card == "VKPay") {
         if (sumperevod > 1500000 || sumperevod + sumpredperevod > 4000000) {
             limitPerevod = false
-            return 0.0
         }
     }
-    var pay = 0.0
-    if ((card == "Mastercard" || card == "Maestro") && sumperevod <= 7500000) {
-        return pay
-    }
-    if ((card == "Mastercard" || card == "Maestro") && sumperevod > 7500000) {
-        pay = sumperevod * 0.6 / 100 + 2000
-        return pay
-    }
-    if (card == "Visa" || card == "Мир") {
-        pay = sumperevod * 0.75 / 100
-        if (pay < 3500) {
-            pay = 3500.0
-        }
-        return pay
-    }
-    return if (card == "VKPay") pay else 0.0
+    return limitPerevod
 }
+
+
 
---
+++ src/test/kotlin/AmountKtTest.kt
@@ -0,0 +1,204 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AmountKtTest {
+
+    @Test
+    fun analizCardVisa() {
+
+
+        // arrange
+
+        val itemCard = "Visa"
+        val itemSumPerevod = 15000
+        val itemSumPredPerevod = 1000
+
+
+        // act
+        val result = analizCard(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(11250, result)
+    }
+
+    @Test
+    fun analizCardMir() {
+
+
+        // arrange
+
+        val itemCard = "Мир"
+        val itemSumPerevod = 25000
+        val itemSumPredPerevod = 1500
+
+
+        // act
+        val result = analizCard(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(18750, result)
+    }
+
+    @Test
+    fun analizCardMastercard() {
+
+
+        // arrange
+
+        val itemCard = "Mastercard"
+        val itemSumPerevod = 76000
+        val itemSumPredPerevod = 1000
+
+
+        // act
+        val result = analizCard(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(47600, result)
+    }
+
+    @Test
+    fun analizCardMaestro() {
+
+
+        // arrange
+
+        val itemCard = "Maestro"
+        val itemSumPerevod = 70000
+        val itemSumPredPerevod = 1000
+
+
+        // act
+        val result = analizCard(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun analizLimitPerevodMaestro() {
+
+
+        // arrange
+
+        val itemCard = "Maestro"
+        val itemSumPerevod = 150000
+        val itemSumPredPerevod = 2000
+
+
+        // act
+        val result = analizLimitPerevod(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun analizLimitPerevodSchet() {
+
+
+        // arrange
+
+        val itemCard = "VKPay"
+        val itemSumPerevod = 15000
+        val itemSumPredPerevod = 2000
+
+
+        // act
+        val result = analizLimitPerevod(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun analizLimitPerevodVisa() {
+
+
+        // arrange
+
+        val itemCard = "Visa"
+        val itemSumPerevod = 120000
+        val itemSumPredPerevod = 2000
+
+
+        // act
+        val result = analizLimitPerevod(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun analizLimitPerevodMir() {
+
+
+        // arrange
+
+        val itemCard = "Мир"
+        val itemSumPerevod = 70000
+        val itemSumPredPerevod = 2600
+
+
+        // act
+        val result = analizLimitPerevod(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun analizLimitPerevodMastercard() {
+
+
+        // arrange
+
+        val itemCard = "Mastercard"
+        val itemSumPerevod = 63000
+        val itemSumPredPerevod = 2700
+
+
+        // act
+        val result = analizLimitPerevod(
+            card = itemCard,
+            sumperevod = itemSumPerevod,
+            sumpredperevod = itemSumPredPerevod
+        )
+
+        // assert
+        assertEquals(true, result)
+    }
+}
